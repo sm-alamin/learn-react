edac75efcece1e0952e086a5a73fdf9e5668e906
sm-alamin <sma.fkml@gmail.com>
2022-04-03 21:36:24 +0600

    json data by array index

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,43 @@
 
 import './App.css';
 import ProfileCard from './components/ProfileCard';
+import Data from './Data';
 
 function App() {
   return (
     <div className="App">
       <ProfileCard  
-      profileType ="Pro" 
-      profilePhoto ="https://randomuser.me/api/portraits/women/79.jpg"
-       profileName = "Ricky Park"
-     profilePlace = "New York"
-     profileStatus1 = "User interface designer and "
-     profileStatus2 = " front-end developer"
-     profileMessage = "Message"
-     profileFollowing = "Following"
-     skills ={[
-      "UI/ UX ",
-      "Frontend Development ",
-      "Html ",
-      "Php ",
-      "Laravel"
-     ]
-     }
-      
+      profileType ={Data[0].profileType} 
+      profilePhoto ={Data[0].profilePhoto} 
+       profileName = {Data[0].profileName} 
+     profilePlace ={Data[0].profilePlace} 
+     profileStatus1 = {Data[0].profileStatus1} 
+     profileStatus2 = {Data[0].profileStatus2} 
+     profileMessage = {Data[0].profileMessage} 
+     profileFollowing = {Data[0].profileFollowing} 
+     skills = {Data[0].skills} 
       />
        <ProfileCard  
-      profileType ="Pro" 
-      profilePhoto ="https://randomuser.me/api/portraits/women/79.jpg"
-       profileName = "Ricky Park"
-     profilePlace = "New York"
-     profileStatus1 = "User interface designer and "
-     profileStatus2 = " front-end developer"
-     profileMessage = "Message"
-     profileFollowing = "Following"
-     skills ={[
-      "UI/ UX ",
-      "Frontend Development ",
-      "Html ",
-      "Php ",
-      "Laravel"
-     ]
-     }
-      
+      profileType ={Data[1].profileType} 
+      profilePhoto ={Data[1].profilePhoto} 
+       profileName = {Data[1].profileName} 
+     profilePlace ={Data[1].profilePlace} 
+     profileStatus1 = {Data[1].profileStatus1} 
+     profileStatus2 = {Data[1].profileStatus2} 
+     profileMessage = {Data[1].profileMessage} 
+     profileFollowing = {Data[1].profileFollowing} 
+     skills = {Data[1].skills} 
+      />
+     <ProfileCard  
+      profileType ={Data[2].profileType} 
+      profilePhoto ={Data[2].profilePhoto} 
+       profileName = {Data[2].profileName} 
+     profilePlace ={Data[2].profilePlace} 
+     profileStatus1 = {Data[2].profileStatus1} 
+     profileStatus2 = {Data[2].profileStatus2} 
+     profileMessage = {Data[2].profileMessage} 
+     profileFollowing = {Data[2].profileFollowing} 
+     skills = {Data[2].skills} 
       />
     
     </div>
