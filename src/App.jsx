94c71d496639962ec003172f202b16f70ebabd87
sm-alamin <sma.fkml@gmail.com>
2022-04-03 21:57:59 +0600

    json data shows by for loop

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,23 @@ import ProfileCard from './components/ProfileCard';
 import Data from './Data';
 
 function App() {
+  let items =[];
+  for (let i =0; i < Data.length ; i++) {
+    items.push(<ProfileCard  
+      profileType ={Data[i].profileType} 
+      profilePhoto ={Data[i].profilePhoto} 
+       profileName = {Data[i].profileName} 
+     profilePlace ={Data[i].profilePlace} 
+     profileStatus1 = {Data[i].profileStatus1} 
+     profileStatus2 = {Data[i].profileStatus2} 
+     profileMessage = {Data[i].profileMessage} 
+     profileFollowing = {Data[i].profileFollowing} 
+     skills = {Data[i].skills} 
+      />)
+  }
   return (
     <div className="App">
-      <ProfileCard  
-      profileType ={Data[0].profileType} 
-      profilePhoto ={Data[0].profilePhoto} 
-       profileName = {Data[0].profileName} 
-     profilePlace ={Data[0].profilePlace} 
-     profileStatus1 = {Data[0].profileStatus1} 
-     profileStatus2 = {Data[0].profileStatus2} 
-     profileMessage = {Data[0].profileMessage} 
-     profileFollowing = {Data[0].profileFollowing} 
-     skills = {Data[0].skills} 
-      />
-       <ProfileCard  
-      profileType ={Data[1].profileType} 
-      profilePhoto ={Data[1].profilePhoto} 
-       profileName = {Data[1].profileName} 
-     profilePlace ={Data[1].profilePlace} 
-     profileStatus1 = {Data[1].profileStatus1} 
-     profileStatus2 = {Data[1].profileStatus2} 
-     profileMessage = {Data[1].profileMessage} 
-     profileFollowing = {Data[1].profileFollowing} 
-     skills = {Data[1].skills} 
-      />
-     <ProfileCard  
-      profileType ={Data[2].profileType} 
-      profilePhoto ={Data[2].profilePhoto} 
-       profileName = {Data[2].profileName} 
-     profilePlace ={Data[2].profilePlace} 
-     profileStatus1 = {Data[2].profileStatus1} 
-     profileStatus2 = {Data[2].profileStatus2} 
-     profileMessage = {Data[2].profileMessage} 
-     profileFollowing = {Data[2].profileFollowing} 
-     skills = {Data[2].skills} 
-      />
+     {items}
     
     </div>
   );
